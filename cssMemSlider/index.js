725fd81c8cc65c704a9f1dcec0594832387ff52a
sasaafanasienka <you@example.comafanasienka@yandex.by>
2021-10-16 15:48:36 +0300

    feat: text block

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -1,18 +1,32 @@
+const texts = {
+    0: 'First meme',
+    1: 'Second meme',
+    2: 'Third meme',
+    3: 'Fourth meme',
+    4: 'Last meme',
+}
+
 class Slider {
     constructor() {
         this.buttons = document.querySelectorAll('.button')
         this.slider = document.querySelector('.slider')
+        this.item = document.querySelector('.item')
+        this.text = document.querySelector('.text')
 
-        this.changeimage = this.changeimage.bind(this)    }
+        this.changeImage = this.changeImage.bind(this)    
+        this.changeText = this.changeText.bind(this)    
+    }
 
     init() {
+        this.text.textContent = texts[0]
         this.buttons.forEach((button, index) => {
-            button.addEventListener('click', () => {this.changeimage(index)})
+            button.addEventListener('click', () => {this.changeImage(index)})
         })
     }
 
-    changeimage(index) {
-        this.slider.style.transform = `translateX(-${500 * index}px)`
+    changeImage(index) {
+        const step = this.item.offsetWidth
+        this.slider.style.transform = `translateX(-${step * index}px)`
         this.buttons.forEach((button, i) => {
             if (index == i) {
                 button.classList.add('button_active')
@@ -20,7 +34,15 @@ class Slider {
                 button.classList.remove('button_active')
             }
         })
-        // console.log(index)
+        this.changeText(index)
+    }
+
+    changeText(index) {
+        this.text.classList.add('text_disabled')
+        setTimeout(() => {
+            this.text.textContent = texts[index]
+            this.text.classList.remove('text_disabled')
+        }, 200)
     }
 }
 
